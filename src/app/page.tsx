2320acdddeaa03970fceda59f8d013ac8a369929
janddrras <janddrras@gmail.com>
2024-05-15 10:04:00 +0300

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import Image from "next/image"
+import CallToAction from "@/components/front-page/CallToAction"
+import HeroSection from "@/components/front-page/HeroSection"
+import ReviewsSection from "@/components/front-page/ReviewsSection"
 
 export default function Home() {
-  return <div>p</div>
+  return (
+    <div className="bg-slate-50 grainy-light">
+      <HeroSection />
+
+      <ReviewsSection />
+
+      <CallToAction />
+    </div>
+  )
 }
