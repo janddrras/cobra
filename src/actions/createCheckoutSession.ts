59fd977f95e0b097d2e4bcdbf9dc8b961f0e6e47
janddrras <janddrras@gmail.com>
2024-05-19 00:57:49 +0300

    Auth modal

--- src/actions/createCheckoutSession.ts
+++ src/actions/createCheckoutSession.ts
@@ -48,7 +48,7 @@ export const createCheckoutSession = async ({ configId }: { configId: string }) 
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
     payment_method_types: ["card", "paypal"],
     mode: "payment",
-    shipping_address_collection: { allowed_countries: ["DE", "US"] },
+    shipping_address_collection: { allowed_countries: ["RO", "US"] },
     metadata: {
       userId: user.id,
       orderId: order.id
--- src/components/configure-page/DesignPreview.tsx
+++ src/components/configure-page/DesignPreview.tsx
@@ -9,6 +9,7 @@ import Phone from "../Phone"
 import Confetti from "react-dom-confetti"
 import usePreviewAndPayment from "@/hooks/usePreviewAndPayment"
 import { useEffect, useState } from "react"
+import LoginModal from "../LoginModal"
 
 interface DesignPreviewProps {
   configuration: Configuration
@@ -29,7 +30,7 @@ const DesignPreview = ({ configuration }: DesignPreviewProps) => {
         <Confetti active={showConfetti} config={{ elementCount: 200, spread: 90 }} />
       </div>
 
-      {/* <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} /> */}
+      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} />
 
       <div className="mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2">
