71f2a12ddc4affa0b3ebc8afbe6721ffd2c82f1f
janddrras <janddrras@gmail.com>
2024-05-21 11:51:16 +0300

    dasboard page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,12 @@
+import ThankYou from "@/components/thank-you-page/ThankYou"
+import { Suspense } from "react"
+
+const Page = () => {
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  )
+}
+
+export default Page
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -56,6 +56,6 @@ export function constructMetadata({
       creator: "@joshtriedcoding"
     },
     icons,
-    metadataBase: new URL("https://casecobra.vercel.app/")
+    metadataBase: new URL("https://cobra-rho.vercel.app/")
   }
 }
